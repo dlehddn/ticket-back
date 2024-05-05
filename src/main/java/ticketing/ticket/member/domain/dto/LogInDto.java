34dc0ea430cd20a8fc3dabd93f3d7344296b80ad
woo <ldw28517@naver.com>
2024-05-05 22:59:01 +0900

    refactor: member 관련 dto 수정

--- src/main/java/ticketing/ticket/member/domain/dto/LogInDto.java
+++ src/main/java/ticketing/ticket/member/domain/dto/LogInDto.java
@@ -1,10 +1,8 @@
 package ticketing.ticket.member.domain.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class LogInDto {
     private String email;
     private String password;
