fec2aba09be79d12f5b158599bc079417e960d65
woo <ldw28517@naver.com>
2024-05-05 22:45:19 +0900

    refactor: member 관련 dto 수정

--- src/main/java/ticketing/ticket/member/domain/entity/Member.java
+++ src/main/java/ticketing/ticket/member/domain/entity/Member.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
 import ticketing.ticket.common.entity.BaseEntity;
 import ticketing.ticket.membercoupon.domain.entity.MemberCoupon;
 
