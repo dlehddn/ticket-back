bce6eda111725d71e9afef1b8915c0ee14f72e84
woo <ldw28517@naver.com>
2024-05-07 23:56:33 +0900

    refactor: Response DTO에 엔티티를 필드로 넣어놨던 부분 수정

--- src/main/java/ticketing/ticket/reservation/domain/dto/MemSeatReservationResponseDto.java
+++ src/main/java/ticketing/ticket/reservation/domain/dto/MemSeatReservationResponseDto.java
@@ -5,10 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 import ticketing.ticket.reservation.domain.entity.SeatReservation;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @Builder
 public class MemSeatReservationResponseDto {
-    private SeatReservation seatReservation;
+    private String category;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String artist;
+    private String seatName;
+    private String seatGrade;
     private Integer totalPrice;
 }
